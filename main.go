98a7bc3e2e4502a7a668e539b38c1eb51849a144
Nathan Owens <nmowens95@gmail.com>
2023-10-07 18:12:05 -0500

    Not functional, still working on getting the interface and struct down. Need to look into other ways for user input.

--- main.go
+++ main.go
@@ -3,5 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	for {
+		fmt.Println("Type in a command or q to exit: ")
+		var input string
+		fmt.Scanln(&input)
+		if input == "q" {
+			break
+		} else {
+			continue
+		}
+	}
+}
+
+type PDex interface {
+	commandHelp()
+	commandExit()
+}
+
+type cliCommand struct {
+		name string
+		description string
+		callback func() error
+}
+
+func commandHelp() string {
+	
+}
+
+return map[string]cliCommand{
+	"help": {
+		name: "help",
+		desription: "Display a help message",
+		callback: commandHelp,
+	},
+	"exit": {
+		name: "exit",
+		description: "Exit the Pokedex"
+		callback: commandExit,
+	},
 }
