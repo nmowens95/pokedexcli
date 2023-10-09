00317fc944ae4a7754cd414a213750ac3b0ef0aa
nmowens95 <nmowens95@gmail.com>
2023-10-09 15:29:05 -0500

    Added logic to commands, as well as created a map to the commands with a callback function

--- repl.go
+++ repl.go
@@ -11,10 +11,26 @@ func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		fmt.Print("Pokedex > ")
+		fmt.Println("Pokedex > ")
 		scanner.Scan()
-		input := scanner.Text()
-		fmt.Printf("Youre text was: %v \n", input)
+		text := scanner.Text()
+		words := cleanInput(text)
+		if len(words) == 0 {
+			continue
+		}
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
 }
 
@@ -24,4 +40,24 @@ func cleanInput(str string) []string {
 	return words
 }
 
+type cliCommand struct {
+	name		string
+	description string
+	callback	func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand {
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+		"exit": {
+			name: "exit",
+			description: "Exit the program",
+			callback: commandExit,
+		},
+	}
+}
 
