9d454d446b9eab819465d6695186501f171a3108
nmowens95 <nmowens95@gmail.com>
2023-10-27 15:24:14 -0500

    Reverted changes on main so that it only runs startRepl again

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/nmowens95/pokedexcli/internal/pokeapi"
-)
-
 func main() {
-	pokeapiClient := pokeapi.NewClient()
-
-	resp, err := pokeapiClient.ListLocationAreas()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(resp)
-	//startRepl()
+	startRepl()
 }
