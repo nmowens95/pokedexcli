7b7821544fff1ab6445c5419f69ea203385205c4
nmowens95 <nmowens95@gmail.com>
2023-10-28 19:39:30 -0500

    Added a config struct to be able to implement a command of moving forward or back within the map locations

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import "github.com/nmowens95/pokedexcli/internal/pokeapi"
+
+type config struct {
+	pokeapiClient       pokeapi.Client
+	nextLocationAreaURL *string
+	prevLocationAreaURL *string
+}
+
 func main() {
-	startRepl()
+	cfg := config{
+		pokeapiClient: pokeapi.NewClient(),
+	}
+
+	startRepl(&cfg)
 }
