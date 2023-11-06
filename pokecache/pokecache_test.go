ae32a12c647463e96f0bcb5b22eb07c51527ab9e
nmowens95 <nmowens95@gmail.com>
2023-11-05 18:57:56 -0600

    set up tests for cacheing

---
+++ pokecache/pokecache_test.go
@@ -0,0 +1,46 @@
+package pokecache
+
+import "testing"
+
+func TestCreateCache(t *testing.T) {
+	cache := newCache()
+	if cache.cache == nil {
+		t.Error("cache is nil")
+	}
+}
+
+func TestAddGetCache(t *testing.T) {
+	cache := newCache()
+
+	cases := []struct {
+		inputKey string
+		inputVal []byte
+	}{
+		{
+			inputKey: "key1",
+			inputVal: []byte("val1"),
+		},
+		{
+			inputKey: "key2",
+			inputVal: []byte("val2"),
+		},
+		{
+			inputKey: "",
+			inputVal: []byte("val3"),
+		},
+	}
+
+	for _, cas := range cases {
+		cache.Add(cas.inputKey, cas.inputVal)
+		actual, ok := cache.Get(cas.inputKey)
+		if !ok {
+			t.Errorf("%s not found", cas.inputKey)
+			continue
+		}
+		if string(actual) != string(cas.inputVal) {
+			t.Errorf("%s does not match %s", string(actual), string(cas.inputVal))
+			continue
+		}
+	}
+
+}
