87066ad5136074551f94a681cf2f13868566f0ec
nmowens95 <nmowens95@gmail.com>
2023-11-08 18:00:13 -0600

    Now have logic and error handling around catching each pokemon, the threshold can be played with. It works in a way that there will be a random probability based on the pokemons baseXP and if our given threshold can match the random probability

---
+++ command_catch.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+func callbackCatch(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("no new pokemon provided")
+	}
+	pokemonName := args[0]
+
+	pokemon, err := cfg.pokeapiClient.GetPokemon(pokemonName)
+	if err != nil {
+		return err
+	}
+
+	const threshold = 50
+	randNum := rand.Intn(pokemon.BaseExperience)
+	fmt.Println(pokemon.BaseExperience, randNum, threshold)
+	if randNum > threshold {
+		return fmt.Errorf("failed to catch %s", pokemonName)
+	}
+	fmt.Printf("Pokemon %s was caught!\n", pokemonName)
+	return nil
+}
